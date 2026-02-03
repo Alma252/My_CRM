f5a719545eb8d531607149f6c5253935449cdc50
Alma252 <shydaskry6@gmail.com>
2026-02-03 21:20:36 +0330

    changes in common/models.py .add base.py and manager.py

--- crm/common/models.py
+++ crm/common/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.conf import settings
 
-# Create your models here.
+
+from .managers import UserManager
+from .base import BaseModel
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, blank=True, null =True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
